5cee83609497e864eb711bfdabc68bfad19f2dc0
Chris Pravetz <chris@pravetz.net>
2021-08-20 11:40:26 -0400

    update dependencies to 2.3

--- package.js
+++ package.js
@@ -1,13 +1,13 @@
 Package.describe({
-	name: "sergeyt:typeahead",
+	name: "seakaytee:typeahead",
 	summary: "Autocomplete package for meteor powered by twitter typeahead.js",
 	git: "https://github.com/sergeyt/meteor-typeahead.git",
 	version: "1.2.0"
 });
 
 Package.onUse(function(api) {
-	api.versionsFrom('METEOR@1.2');
-	api.use(['jquery', 'blaze@1.0.0 || 2.0.0', 'ui'], 'client');
+	api.versionsFrom(['METEOR@1.2','2.3']);
+	api.use(['jquery@3.0.0', 'blaze', 'ui'], 'client');
 	api.use('twbs:bootstrap@3.0.0', 'client', {weak: true});
 	api.addFiles('typeahead.bundle.js', 'client');
 	api.addFiles('index.js', 'client');
